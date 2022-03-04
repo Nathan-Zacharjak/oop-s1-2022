7fe82c8511aab75718d0e0f4550ec9b9935a9fdc
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-04 16:06:54 +1030

    Added q1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -2,4 +2,15 @@
 double average(int array[], int n)
 {
 	// your code goes here
+
+	if (n<1){
+		return 0.0;
+	}
+
+	double sum = 0;
+	for (int i = 0; i < n; i++){
+		sum = sum + array[i];
+	}
+
+	return sum / n;
 }
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -3,9 +3,16 @@
 
 extern double average(int*,int);
 
+using namespace std;
+
 int main(int argc,char **argv)
 {
 	// your code goes here ...
+	int array[] = {1,2,3,4};
+	int size = 4;
+
+	cout << average(array,size) << endl;
+	cout << average(array,0) << endl;
 	return 0 ;
 }
 
