e202e8fb04e869f1c6a4e79cf3b2668a226cc965
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-09 16:14:49 +1030

    Fixed q2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -8,6 +8,8 @@ using namespace std;
 void print_as_binary(string decimal_number){
 	// Convert the given string to a number
 	int number = stoi(decimal_number);
+	// If the number is 0, nothing gets put into the vector
+	// So just print 0 for this special case
 	if (number == 0){
 		cout << 0 << endl;
 		return;
@@ -24,10 +26,16 @@ void print_as_binary(string decimal_number){
 		number = number/2;
 	}
 	
+	bool printedOne = false;
+
 	// This method of getting a binary number gives it to you in reverse
 	// So, the array of ints of the binary number is printed out in reverse
 	for (int i = binary.size(); i >= 0; i--){
-		cout << binary[i];
+		if (printedOne || binary[i] == 1){
+			printedOne = true;
+			cout << binary[i];
+		}
+		
 	}
 	cout << endl;
 
