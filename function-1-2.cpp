fdc0f348060656cb6cbf5d926c7282803ebb84c9
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-09 16:45:01 +1030

    Added q1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,16 +1,14 @@
 // function to ...
-double average(int array[], int n)
-{
-	// your code goes here
-
-	if (n<1){
-		return 0.0;
+int identity(int array[10][10]){
+	for (int i = 0; i < 10; i++){
+		for (int j = 0; j < 10; j++){
+			if (i == j && array[i][j] != 1){
+				return 0;
+			}else if (i != j && array[i][j] != 0){
+				return 0;
+			}
+		}
 	}
 
-	double sum = 0;
-	for (int i = 0; i < n; i++){
-		sum = sum + array[i];
-	}
-
-	return sum / n;
+	return 1;
 }
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <stdlib.h>
 
-extern double average(int*,int);
+extern int identity(int [10][10]);
 
 using namespace std;
 
-int main(int argc,char **argv)
-{
+int main(int argc,char **argv){
 	// your code goes here ...
-	int array[] = {1,2,3,4};
-	int size = 4;
+	int array[10][10] = {{1,0,0,0,0,0,0,0,0,0},
+						 {0,1,0,0,0,0,0,0,0,0},	
+						 {0,0,1,0,0,0,0,0,0,0},	
+						 {0,0,0,1,0,0,0,0,0,0},	
+						 {0,0,0,0,1,0,0,0,0,0},	
+						 {0,0,0,0,0,1,0,0,0,0},	
+						 {0,0,0,0,0,0,1,0,0,0},	
+						 {0,0,0,0,0,0,0,1,0,0},	
+						 {0,0,0,0,0,0,0,0,1,0},	
+						 {0,0,0,0,0,0,0,0,0,1},	
+	};
 
-	cout << average(array,size) << endl;
-	cout << average(array,0) << endl;
+	cout << identity(array) << endl;
 	return 0 ;
 }
 
