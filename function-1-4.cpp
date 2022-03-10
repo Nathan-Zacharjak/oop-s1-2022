6c12966d80971d6785e7b9855822e77e2c421eb3
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-10 13:51:50 +1030

    fixed section 1 questions

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -10,7 +10,10 @@ void print_scaled_matrix(int array[3][3],int scale){
 	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
 			array[i][j] *= scale;
-			cout << array[i][j] << " ";
+			cout << array[i][j];
+			if (j != 3) {
+				cout << " ";
+			}
 		}
 		cout << endl;
 	}
--- function-1-5.cpp
+++ function-1-5.cpp
@@ -8,7 +8,10 @@ void print_summed_matrices(int array1[3][3],int array2[3][3]){
 	// Your code goes here ...
 	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
-			cout << array1[i][j] + array2[i][j] << " ";
+			cout << array1[i][j] + array2[i][j];
+			if (j != 3) {
+				cout << " ";
+			}
 		}
 		cout << endl;
 	}
