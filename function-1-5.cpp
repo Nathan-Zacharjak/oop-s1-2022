f46fe5682c0de90e242cb76c6246641d51e84593
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-10 13:53:20 +1030

    fixed section 1 questions

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -9,7 +9,7 @@ void print_summed_matrices(int array1[3][3],int array2[3][3]){
 	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
 			cout << array1[i][j] + array2[i][j];
-			if (j != 3) {
+			if (j != 2) {
 				cout << " ";
 			}
 		}
