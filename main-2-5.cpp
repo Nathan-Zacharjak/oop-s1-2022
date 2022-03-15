ed27db041d052f201aeb4f3a7e2379da37b20793
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-15 14:25:48 +1030

    fixed q2-5

--- main-2-5.cpp
+++ main-2-5.cpp
@@ -8,17 +8,17 @@ int main(int argc,char **argv){
                           {5.5,6.6,7.7,8.8},
                           {9,10,11,12},
                           {13,14,15,16}};
+    int rowCount = 4;
+    int columnCount = 4;
 
-    cout << array[1] << endl;
-    // int pointers = {array[1],array[2],array[3],array[4]};
+    for (int i = 0; i < rowCount; i++){
+        double* rowPointer = *(array + i);
 
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            
+        for (int j = 0; j < columnCount; j++){
+            cout << *(rowPointer + j) << " ";
         }
         
+        cout << endl;
     }
     
 
