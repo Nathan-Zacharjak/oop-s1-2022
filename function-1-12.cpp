cb4eb12af88902a5ef607553ac4741e0ff7b96ee
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-21 16:58:34 +1030

    fix parts 1 and 2

--- function-1-12.cpp
+++ function-1-12.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 int size_of_variable_star_arr(){
     int *arr = new int[3];
-    arr[1] = 3;
-    cout << arr[1] << endl;
     delete [] arr;
-    // arr = nullptr;
-    // cout << arr[1] << endl;
     return sizeof(*arr);
 }
--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,4 +1,4 @@
 int size_of_variable_star_arr(){
     int *arr = new int[3];
-    return sizeof(*arr);
+    return sizeof(arr);
 }
