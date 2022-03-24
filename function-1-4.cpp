b22bee712bf24828c6179e0a577427ca89109f8c
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-24 16:01:47 +1030

    added q 1-4

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -6,11 +6,4 @@ void cpyda(double *old_array,double *new_array,int length) {
     for (int i = 0; i < length; i++){
         *(new_array + i) = *(old_array + i);
     }
-    
-    for (int i = 0; i < length; i++){
-        cout << new_array[i] << endl;
-    }
-    
-
-    return;
 }
