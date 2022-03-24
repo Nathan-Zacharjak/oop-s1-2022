bf041664876c3425b432d4245dcfe14ca409c9da
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-24 16:57:01 +1030

    added q 2-2

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -5,8 +5,9 @@ using namespace std;
 extern int maximum_sum(int *nums,int length);
 
 int main(int argc, char const *argv[]){
-    int array[] = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
-    int length = 10;
+    int array[] = { 31, -41, 59, 26, -53};
+    int length = 5;
+    
 
     cout << maximum_sum(array, length) << endl;
 
