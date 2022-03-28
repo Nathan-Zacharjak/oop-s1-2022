2ac4be33cf7bd06d157e50b8e3c08a56df2a569a
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-03-28 15:36:28 +1030

    added q2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
-
 using namespace std;
 
-void print_sevens(int *nums,int length){
+int* readNumbers(){
+    int length = 10;
+    int* numbers = new int[length];
+
     for (int i = 0; i < length; i++){
-        if (*(nums + i) % 7 == 0){
-            cout << *(nums + i) << endl;
-        }
-        
+        cin >> numbers[i];
     }
     
+    return numbers;
+}
+
+void hexDigits(int *numbers,int length){
+    for (int i = 0; i < length; i++){
+        cout << i << " " << numbers[i] << " ";
+
+        switch (numbers[i]){
+            case 10:
+                cout << "A";
+                break;
+            case 11:
+                cout << "B";
+                break;
+            case 12:
+                cout << "C";
+                break;
+            case 13:
+                cout << "D";
+                break;
+            case 14:
+                cout << "E";
+                break;
+            case 15:
+                cout << "F";
+                break;
+            default:
+                cout << numbers[i];
+                break;
+        }
+
+        cout << endl;
+    }
 }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -2,22 +2,15 @@
 
 using namespace std;
 
-extern void print_sevens(int *nums,int length);
+extern int* readNumbers();
+extern void hexDigits(int* numbers, int length);
 
 int main(int argc, char const *argv[]){
-    int array[5] = {1,2,3,4,5};
-    int* pointer = array;
-    int length = 5;
-    int array2[5] = {7,49,14,7,1};
-    int* pointer2 = array2;
-    int length2 = 5;
-    int array3[0] = {};
-    int* pointer3 = array3;
-    int length3 = 0;
+    int* numbers = readNumbers();
+    
+    hexDigits(numbers, 10);
 
-    print_sevens(pointer, length);
-    print_sevens(pointer2, length2);
-    print_sevens(pointer3, length3);
+    delete[] numbers;
 
     return 0;
 }
