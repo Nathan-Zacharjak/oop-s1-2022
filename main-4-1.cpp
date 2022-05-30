9f7356b03e184d2e62045bd65d8e56e7a5b4bf31
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-05-30 17:09:09 +0930

    started q4

--- main-4-1.cpp
+++ main-4-1.cpp
@@ -4,6 +4,7 @@
 #include "hunter.h"
 #include "vegie.h"
 #include "zoo.h"
+#include "sort_by_animalID.h"
 
 using namespace std;
 
@@ -14,5 +15,12 @@ int main(){
     cout << zoo1.get_name() << " " << zoo1.get_number_of_animals() << endl;
     cout << daisy1->get_animalID() << " " << daisy1->get_name() << " " << daisy1->get_volume() << endl;
     
+    sort_by_animalID::sort(zoo1.get_animals(), zoo1.get_number_of_animals());
+
+    for (int i = 0; i < zoo1.get_number_of_animals(); i++){
+        cout << zoo1.get_animals()[i]->get_animalID() << endl;
+    }
+    
+
     return 0;
 }
---
+++ sort_by_animalID.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include "sort_by_animalID.h"
+#include "animal.h"
+
+void sort_by_animalID::sort(animal **animals,int n){
+    // for (int i = 0; i < n; i++){
+    //     for (int j = i; i < n - i; i++){
+    //         if (animals[i]->get_animalID() < animals[j]->get_animalID()){
+    //             swap(animals[i], animals[j]);
+    //         }
+            
+    //     }
+        
+    // }
+    
+}
